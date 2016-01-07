bec63345ea722bfebd82ed8a5e0bc0892fd2c000
tsingtao88 <tsingtao88@hotmail.com>
2016-01-07 11:06:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bottle import route, run
 
 @route('/')
 def index():
-    return "<h1> hello OpenShift sta</h1>"
+    return "<h1> Allo OpenShift Team Of LGI</h1>"
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
